ff3e50ef756653d2631ed95cf2d969a70e1303bf
Jhanoo <jhanoo@naver.com>
2023-01-11 01:42:27 +0900

    Ep.02 make Player.cs - Get key input to move

---
+++ Assets/Undead Survivor/Codes/Player.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+
+    public Vector2 inputVec;
+    public float speed;
+
+    Rigidbody2D rigid;
+
+    void Awake() {
+        rigid = GetComponent<Rigidbody2D>();
+    }
+
+    void Update() {
+        inputVec.x = Input.GetAxisRaw("Horizontal");
+        inputVec.y = Input.GetAxisRaw("Vertical");
+    }
+
+    void FixedUpdate() {
+        Vector2 nextVec = inputVec.normalized * speed * Time.fixedDeltaTime;
+
+        rigid.MovePosition(rigid.position + nextVec);
+
+    }
+}
