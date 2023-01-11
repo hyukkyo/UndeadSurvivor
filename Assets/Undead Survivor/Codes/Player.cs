1a44b8969884d821570b60a381295fbc3b7c0fd4
양종문 <2019026599@hanyang.ac.kr>
2023-01-11 21:53:59 +0900

    Ep.03 Make 2D Animation

--- Assets/Undead Survivor/Codes/Player.cs
+++ Assets/Undead Survivor/Codes/Player.cs
@@ -9,19 +9,39 @@ public class Player : MonoBehaviour {
     public float speed;
 
     Rigidbody2D rigid;
+    SpriteRenderer spriter;
+    Animator anim;
 
     void Awake() {
         rigid = GetComponent<Rigidbody2D>();
+        spriter = GetComponent<SpriteRenderer>();
+        anim = GetComponent<Animator>();
+    }
+
+    void Update() {
+        inputVec.x = Input.GetAxisRaw("Horizontal");
+        inputVec.y = Input.GetAxisRaw("Vertical");
     }
 
     void FixedUpdate() {
-        Vector2 nextVec = inputVec * speed * Time.fixedDeltaTime;
-
+        Vector2 nextVec = inputVec.normalized * speed * Time.fixedDeltaTime;
         rigid.MovePosition(rigid.position + nextVec);
 
     }
 
-    void OnMove(InputValue value) {
-        inputVec = value.Get<Vector2>();
+    void LateUpdate() {
+        anim.SetFloat("Speed", inputVec.magnitude);
+
+        if (inputVec.x != 0) {
+            spriter.flipX = inputVec.x < 0;
+        }
     }
+
+    /*
+     * In the lecture3, Lecture02(Moving using the Update function) is used
+     * instead of the Lecture02+(New input system).
+     */
+    // void OnMove(InputValue value) {
+    //     inputVec = value.Get<Vector2>();
+    // }
 }
