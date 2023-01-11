b5954077ec8ef617c795a641a2b9acabed1af181
김채은 <rla_co_dms@naver.com>
2023-01-11 18:23:53 +0900

    Ep.02+ Apply new Input System

--- Assets/Undead Survivor/Codes/Player.cs
+++ Assets/Undead Survivor/Codes/Player.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class Player : MonoBehaviour {
 
@@ -13,15 +14,14 @@ public class Player : MonoBehaviour {
         rigid = GetComponent<Rigidbody2D>();
     }
 
-    void Update() {
-        inputVec.x = Input.GetAxisRaw("Horizontal");
-        inputVec.y = Input.GetAxisRaw("Vertical");
-    }
-
     void FixedUpdate() {
-        Vector2 nextVec = inputVec.normalized * speed * Time.fixedDeltaTime;
+        Vector2 nextVec = inputVec * speed * Time.fixedDeltaTime;
 
         rigid.MovePosition(rigid.position + nextVec);
 
+    }
+
+    void OnMove(InputValue value) {
+        inputVec = value.Get<Vector2>();
     }
 }
