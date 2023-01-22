c41310643d7e2e05977dcc4d4b85fcf3a18e8fc4
Jhanoo <jhanoo@naver.com>
2023-01-22 21:05:44 +0900

    Add upgrade img (weapon 3, 4, 5) & can choose but not applied yet

--- Assets/Undead Survivor/Codes/Level.cs
+++ Assets/Undead Survivor/Codes/Level.cs
@@ -2,41 +2,70 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Level : MonoBehaviour
-{
+public class Level : MonoBehaviour {
     int level = 1;
     int experience = 0;
     [SerializeField] ExperienceBar experienceBar;
     [SerializeField] UpgradeManager upgradePanel;
 
-    int TO_LEVEL_UP
-    {
-        get
-        {
+    [SerializeField] List<UpgradeData> upgrades;
+    List<UpgradeData> selectedUpgrades;
+
+    [SerializeField]List<UpgradeData> acquiredUpgrades;
+
+    int TO_LEVEL_UP {
+        get {
             return level * 1000;
         }
     }
 
-    private void Start()
-    {
+    private void Start() {
         experienceBar.UpdateExperienceSlider(experience, TO_LEVEL_UP);
     }
 
-    public void AddExperience(int amount)
-    {
+    public void AddExperience(int amount) {
         experience += amount;
         CheckLevelUp();
         experienceBar.UpdateExperienceSlider(experience, TO_LEVEL_UP);
     }
 
-    public void CheckLevelUp()
-    {
-        if (experience >= TO_LEVEL_UP)
-        {
-            upgradePanel.OpenUpgradePanel();
+    public void CheckLevelUp() {
+        if (experience >= TO_LEVEL_UP) {
+
+            if (selectedUpgrades == null) {
+                selectedUpgrades = new List<UpgradeData>();
+            }
+            selectedUpgrades.Clear();
+            selectedUpgrades.AddRange(GetUpgrades(3));
+
+            upgradePanel.OpenUpgradePanel(GetUpgrades(3));
             experience -= TO_LEVEL_UP;
             level += 1;
         }
     }
 
+    public void Upgrade(int selectedUpgradeID) {
+        UpgradeData upgradeData = selectedUpgrades[selectedUpgradeID];
+
+        if (acquiredUpgrades == null) {
+            acquiredUpgrades = new List<UpgradeData>();
+        }
+
+        acquiredUpgrades.Add(upgradeData);
+        upgrades.Remove(upgradeData);
+    }
+
+    public List<UpgradeData> GetUpgrades(int count) {
+        List<UpgradeData> upgradeList = new List<UpgradeData>();
+
+        if (count > upgrades.Count) {
+            count = upgrades.Count;
+        }
+
+        for (int i = 0; i < count; i++) {
+            upgradeList.Add(upgrades[Random.Range(0, upgrades.Count)]);
+        }
+
+        return upgradeList;
+    }
 }
--- Assets/Undead Survivor/Codes/UpgradeManager.cs
+++ Assets/Undead Survivor/Codes/UpgradeManager.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -6,15 +7,44 @@ public class UpgradeManager : MonoBehaviour {
 
     [SerializeField] GameObject upgradePanel;
 
-    public void OpenUpgradePanel() {
+    [SerializeField] List<UpgradeButton> upgradeButtons;
+
+    private void Start() {
+        HideButtons();
+    }
+
+    public void OpenUpgradePanel(List<UpgradeData> upgradeDatas) {
+        Clean();
         Time.timeScale = 0f;
         upgradePanel.SetActive(true);
+
+        for (int i = 0; i < upgradeDatas.Count; i++) {
+            upgradeButtons[i].gameObject.SetActive(true);
+            upgradeButtons[i].Set(upgradeDatas[i]);
+        }
+    }
+
+    public void Clean() {
+        for (int i = 0; i < upgradeButtons.Count; i++) {
+            upgradeButtons[i].Clean();
+        }
+    }
+
+    public void Upgrade(int pressedButtonID) {
+        GameManager.instance.player.GetComponent<Level>().Upgrade(pressedButtonID);
+        CloseUpgradePanel();
     }
 
     public void CloseUpgradePanel() {
+        HideButtons();
+
         Time.timeScale = 1f;
         upgradePanel.SetActive(false);
     }
 
-
+    public void HideButtons() {
+        for (int i = 0; i < upgradeButtons.Count; i++) {
+            upgradeButtons[i].gameObject.SetActive(false);
+        }
+    }
 }
